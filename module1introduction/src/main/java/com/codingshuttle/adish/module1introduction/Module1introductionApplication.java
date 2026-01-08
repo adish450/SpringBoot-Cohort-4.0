9ea6e44565084c26cd827d42f9259ebfcc095602
adish17 <adishirfan17@gmail.com>
2026-01-08 22:11:47 +0530

    using @Component for creating bean and @Autowired for DI

--- module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/Module1introductionApplication.java
+++ module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/Module1introductionApplication.java
@@ -1,17 +1,22 @@
 package com.codingshuttle.adish.module1introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module1introductionApplication {
+public class Module1introductionApplication implements CommandLineRunner {
+
+	@Autowired
+	PaymentService paymentService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Module1introductionApplication.class, args);
-
-		PaymentService paymentService = new PaymentService();
-
-		paymentService.pay();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		paymentService.pay();
+	}
 }
--- module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/PaymentService.java
+++ module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/PaymentService.java
@@ -1,5 +1,10 @@
 package com.codingshuttle.adish.module1introduction;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Component
 public class PaymentService {
 
     public void pay() {
