f692d5c9d283cd0ac3edf95604e69eda5a15177a
adish17 <adishirfan17@gmail.com>
2026-01-08 22:16:26 +0530

    using @Component @Service @Controller @Repository @RestController for creating bean

--- module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/PaymentService.java
+++ module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/PaymentService.java
@@ -1,8 +1,16 @@
 package com.codingshuttle.adish.module1introduction;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component
+/*@Repository
+@Service
+@Controller
+@RestController*/
 public class PaymentService {
 
     public void pay() {
