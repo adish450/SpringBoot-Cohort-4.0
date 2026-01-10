9e0164bce4e12093fb02020eba524613f89ea7ca
adish17 <adishirfan17@gmail.com>
2026-01-10 23:52:27 +0530

    construction injection resolution using @ConditionalOnMissingBean

--- module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/impl/EmailNotificationService.java
+++ module1introduction/src/main/java/com/codingshuttle/adish/module1introduction/impl/EmailNotificationService.java
@@ -1,11 +1,13 @@
 package com.codingshuttle.adish.module1introduction.impl;
 
 import com.codingshuttle.adish.module1introduction.NotificationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 //@ConditionalOnProperty(name = "notification.type", havingValue = "email")
+@ConditionalOnMissingBean(SmsNotificationService.class)
 public class EmailNotificationService implements NotificationService {
     @Override
     public void send(String message) {
