471e4c63be23dcf7e16b72be58f79efd50fb02b8
adish17 <adishirfan17@gmail.com>
2026-01-13 22:38:44 +0530

    introduced EmployeeDTO and updated EmployeeController to use DTOs

--- springbootwebtutorial/src/main/java/com/springboot/springbootwebtutorial/controllers/EmployeeController.java
+++ springbootwebtutorial/src/main/java/com/springboot/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,28 +1,33 @@
 package com.springboot.springbootwebtutorial.controllers;
 
+import com.springboot.springbootwebtutorial.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
-    @GetMapping("/getSecretMessage")
+    /*@GetMapping("/getSecretMessage")
     public String getSecretMessage() {
         return "Secret Message: Hello World!";
+    }*/
+
+    @GetMapping("/{id}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long id) {
+        return new EmployeeDTO(id, "John", "", 25, LocalDate.now(), true);
     }
 
-    @GetMapping("/employees/{id}")
-    public String getEmployeeById(@PathVariable Long id) {
-        return "Employee with id: " + id;
-    }
-
-    @GetMapping("/employees")
+    @GetMapping
     public String getEmployeesSortBy(@RequestParam(required = false) String sortBy,
     @RequestParam String age) {
         return "Employees sorted by: " + sortBy + " and age: " + age;
     }
 
     @PostMapping("/addEmployee")
-    public String addEmployee(@RequestBody String employee) {
-        return "Employee added: " + employee;
+    public EmployeeDTO addEmployee(@RequestBody EmployeeDTO employee) {
+        employee.setId(100L);
+        return employee;
     }
 }
